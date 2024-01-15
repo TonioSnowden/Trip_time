e6aa0fd7c1b524a77cb8fbf3d584e0142cf4e9be
Jean-Baptiste Conan <jeanbaptiste.conan.jbc@gmail.com>
2024-01-15 08:23:02 +0100

    merged main with stashed changes

--- popup.js
+++ popup.js
@@ -1,3 +1,5 @@
+import scraper from "./scraper";
+
 // Function to save data
 function saveAddress() {
     var address = document.getElementById("addressB").value;
@@ -76,3 +78,66 @@ document.addEventListener('DOMContentLoaded', function() {
     // ... [other event listeners] ...
 });
 
+
+
+//Sender
+
+// Function to send data
+async function sendData() {
+    var addressA = document.getElementById('addressA').value;
+    var addressB = document.getElementById('addressB').value;
+
+    // Construct the data you want to send
+    var dataToSend = {
+        addressA: addressA,
+        addressB: addressB
+    };
+
+    // Make the POST request
+    await fetch('https://jbccc.pythonanywhere.com', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(dataToSend),
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('Success:', data);
+        data.durations.forEach((element, i) => {
+            document.getElementById('duration' + i).innerHTML = element;
+        });
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+        // Handle errors here
+    });
+    
+}
+
+// Add event listener for the send button
+document.addEventListener('DOMContentLoaded', function() {
+    var sendButton = document.getElementById('sendButton');
+    sendButton.addEventListener('click', sendData);
+    // ... [other event listeners] ...
+});
+
+
+function testScraper() {
+    const currentUrl = window.location.href;
+    const isFlatfox = currentUrl.includes("flatfox.ch");
+    if (!isFlatfox) {
+        console.log("Not on flatfox.ch");
+        return "Not on flatfox.ch";
+    }
+    console.log(google);
+    const id = currentUrl.split("/").pop();
+    console.log("id: " + id);
+    const data = scraper.flatfox(id);
+    console.log(data);
+    document.getElementsByTagName("p")[0].innerHTML = "oui";
+    return data;
+}
+
+// Event listener for test button
+document.getElementById("testButton").addEventListener("click", testScraper);
